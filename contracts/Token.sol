66bd337b876411b1c5807181bb805915f8c3cf3d
pawan kalyan <pavankalyan4028@gmail.com>
2024-08-01 06:33:34 +0000

    feat: initiated the project

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
-    constructor()ERC20("c6afc40d1c9e0f6c070ca85c41880d057811d5b70e58bcf14372219321cbbef0","jjk"){} 
+    constructor()ERC20("pawan","pa"){} 
 
     function mint100tokens() public {
         _mint(msg.sender, 100*10**18);
